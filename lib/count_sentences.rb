36fdce727046eb47ef2c521fbb1a1be1ddb82459
Isaac Smith <isaacsmith215@aol.com>
2020-04-02 14:39:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,25 +4,31 @@ class String
   count = 0
   def sentence?
     if self.end_with?(".")
+      return true
       count += 1
     else
       return false
+      count += 0
     end
   end
 
   def question?
     if self.end_with?("?")
+      return true
       count += 1
     else
       return false
+      count += 0
     end
   end
 
   def exclamation?
     if self.end_with?("!")
+      return true
       count += 1
     else
       return false
+      count +=  0
     end
   end
   count
